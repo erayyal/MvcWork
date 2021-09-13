8fc080909f8f5a7697fff68269f1de002fa92220
erayyal <77546881+erayyal@users.noreply.github.com>
2021-09-13 15:05:54 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
